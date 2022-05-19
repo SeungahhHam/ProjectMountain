9c3b1e98ec37df3671e45b18bc4856320cde3b69
leejae7124 <83765011+leejae7124@users.noreply.github.com>
2022-05-19 14:50:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,67 @@
+'use strict'
 const express = require('express')
 const app = express()
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const cookieParser = require('cookie-parser')
-app.use(cookieParser());
+app.use(cookieParser())
 
-//init Middleware
-app.use(express.json({extened: false}))
+//use helmet modules for web security
+const helmet = require('helmet')
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 
-//connect DB
+// init Middleware
+app.use(express.json({ extened: false }))
+
+// connect DB
 const connectDB = require('./config/db')
-connectDB();
+connectDB()
 
-//route handling for mountinit
+// route handling for mountinit
 app.use('/api/mountInit', require('./routes/mountInit'))
 
-//route handling for usermanage
+// route handling for usermanage
 app.use('/api/user', require('./routes/usermanage'))
 
-//route handling for community
-app.use('/api/community/free', require('./routes/bordF')) 
-app.use('/api/community/certify', require('./routes/bordC')) 
-app.use('/api/community/question', require('./routes/bordQ')) 
+// route handling for community
+app.use('/api/community/free', require('./routes/bordF'))
+app.use('/api/community/certify', require('./routes/bordC'))
+app.use('/api/community/question', require('./routes/bordQ'))
 app.use('/api/community/recruit', require('./routes/bordR'))
 
-//route handling for mountinfo
+// route handling for mountinfo
 app.use('/api/mountInfo', require('./routes/mountInfo'))
 
-//route handling for unity
+// route handling for unity
 app.use('/api/unity', require('./routes/unity'))
 
-//route handling for imagemanage
-app.use('/api/image', require('./routes/imagemanage'));
+// route handling for imagemanage
+app.use('/api/image', require('./routes/imagemanage'))
 
-//route handling for map
+// route handling for map
 const path = require('path')
 app.use('/api/map', require('./routes/map'))
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-//통신 연습
+
+// 통신 연습
 app.get('/', (req, res) => {
-  res.json({message: 'hi backend!!!!!!!'})
+  res.json({ message: 'hi backend!!!!!!!' })
 })
 
-//Port
+// Port
 const port = 5000
-app.listen(port, () => console.log('Server is running'));
+app.listen(port, () => console.log('Server is running'))
