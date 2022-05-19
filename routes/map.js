f0c88cc0fe6e8458a4d51bebf70cec5cf008abf3
leejae7124 <83765011+leejae7124@users.noreply.github.com>
2022-05-19 14:55:42 +0900

    Update map.js

--- routes/map.js
+++ routes/map.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const router = express.Router();
+'use strict'
+const express = require('express')
+const router = express.Router()
 const key = require('../config/javascriptkey')
 const map = require('../model/loc')
 
 router.post('/set', (req, res, next) => {
-    map.mapSchema.collection.updateOne({token: req.body.token},{$set: {x: req.body.x, y: req.body.y}}, { upsert: true }, function(error, docs){
-        if(error){
-            console.log(error);
-        }else{
-          res.send({message: 'yes'})
-        }
-    })
+  map.mapSchema.collection.updateOne({ token: req.body.token }, { $set: { x: req.body.x, y: req.body.y } }, { upsert: true }, function (error, docs) {
+    if (error) {
+      console.log(error)
+    } else {
+      res.send({ message: 'yes' })
+    }
+  })
 })
 
 router.get('/:token', (req, res, next) => {
-    map.mapSchema.collection.findOne({token: req.params.token}, function(error, docs){
-        if(error){
-            console.log(error);
-        }else{
-            res.render('map', {
-                javascriptkey: key.javascriptkey,
-                x: parseFloat(docs.x),
-                y: parseFloat(docs.y)
-            })
-        }
-    })
+  map.mapSchema.collection.findOne({ token: req.params.token }, function (error, docs) {
+    if (error) {
+      console.log(error)
+    } else {
+      res.render('map', {
+        javascriptkey: key.javascriptkey,
+        x: parseFloat(docs.x),
+        y: parseFloat(docs.y)
+      })
+    }
+  })
 })
 
 module.exports = router
