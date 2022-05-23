a22358c2a5814fa75513694c7cf39db4450ebb5e
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2022-05-23 21:47:59 +0900

    Update community.js

--- model/community.js
+++ model/community.js
@@ -1,40 +1,48 @@
-'use strict'
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-const communitySchema = new Schema({
-  nickname: {
-    type: String
-  },
-  mountname: {
-    type: String
-  },
-  imagepath: {
-    type: String
-  },
-  title: {
-    type: String
-  },
-  userImage: {
-    type: String
-  },
-  text: {
-    type: String
-  },
-  comment: {
-    type: Array
-  },
-  time: {
-    type: String
-  },
-  _id: {
-    type: String
-  }
+var communitySchema = new Schema({
+    nickname: {
+        type: String
+    },
+    mountname: {
+        type: String
+    },
+    imagepath: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    userImage: {
+        type: String
+    },
+    text: {
+        type: String
+    },
+    comment: {
+        type: Array
+    },
+    time: {
+        type: String
+    },
+    gender: {
+        type: String,
+    },
+    num: {
+        type: String,
+    },
+    loc: {
+        type: String
+    },
+    _id: {
+        type: String
+    }
 })
 
 module.exports = {
-  bordF: mongoose.model('bordF', communitySchema),
-  bordQ: mongoose.model('brodQ', communitySchema),
-  bordC: mongoose.model('bordC', communitySchema),
-  bordR: mongoose.model('bordR', communitySchema)
+    bordF : mongoose.model('bordF', communitySchema),
+    bordQ : mongoose.model('brodQ', communitySchema),
+    bordC : mongoose.model('bordC', communitySchema),
+    bordR : mongoose.model('bordR', communitySchema)
 }
